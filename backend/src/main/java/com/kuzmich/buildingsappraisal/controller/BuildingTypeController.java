b4109fda554b40807e59799be81b5567f9d76a9b
Mikhail Kuzmich <kmv85@mail.ru>
2024-11-17 11:28:17 +0300

    update cors policy

--- backend/src/main/java/com/kuzmich/buildingsappraisal/controller/BuildingTypeController.java
+++ backend/src/main/java/com/kuzmich/buildingsappraisal/controller/BuildingTypeController.java
@@ -6,12 +6,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +25,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS})
 @Slf4j
 @RequestMapping("/api")
 public class BuildingTypeController {
