1306ee7366ed5862e4966a2f674eae4a0d036aeb
Mikhail Kuzmich <kmv85@mail.ru>
2025-02-15 15:22:51 +0300

    update Dataloader logic

--- backend/src/main/java/com/kuzmich/buildingsappraisal/repository/BuildingTypeRepository.java
+++ backend/src/main/java/com/kuzmich/buildingsappraisal/repository/BuildingTypeRepository.java
@@ -1,6 +1,5 @@
 package com.kuzmich.buildingsappraisal.repository;
 
-import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -8,6 +7,5 @@ import com.kuzmich.buildingsappraisal.model.BuildingType;
 
 public interface BuildingTypeRepository extends MongoRepository<BuildingType, String>{
 
-    public Optional<BuildingType> findByType(String type);
     
 }
--- backend/src/main/java/com/kuzmich/buildingsappraisal/service/DataLoader.java
+++ backend/src/main/java/com/kuzmich/buildingsappraisal/service/DataLoader.java
@@ -3,40 +3,48 @@ package com.kuzmich.buildingsappraisal.service;
 import java.io.InputStream;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kuzmich.buildingsappraisal.model.BuildingType;
 import com.kuzmich.buildingsappraisal.repository.BuildingTypeRepository;
 
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class DataLoader {
 
-    @Autowired
-    private BuildingTypeRepository repository;
-
-    @Autowired
-    private ResourceLoader resourceLoader;
+    private final BuildingTypeRepository repository;
+    private final ResourceLoader resourceLoader;
 
     @PostConstruct
+    @Transactional
     public void loadData() {
         try {
+            long repositoryCount = repository.count();
+
             InputStream inputStream = resourceLoader.getResource("classpath:data.json").getInputStream();
             ObjectMapper mapper = new ObjectMapper();
             List<BuildingType> entities = List.of(mapper.readValue(inputStream, BuildingType[].class));
-            for(BuildingType entity : entities) {
-                if(repository.findByType(entity.getType()).isEmpty()) {
-                    repository.save(entity);
-                }
+            int fileCount = entities.size();
+
+            if (fileCount != repositoryCount) {
+                log.info("Mismatch detected. Updating database. File count: {}, Repository count: {}", fileCount, repositoryCount);
+                repository.deleteAll();
+                repository.saveAll(entities);
+                log.info("Database update completed successfully.");
+            } else {
+                log.info("No update needed. File count and repository count match: {}", fileCount);
             }
-
-
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Error occurred during data loading", e);
+            throw new RuntimeException("Failed to load data", e);
         }
     }
 }
